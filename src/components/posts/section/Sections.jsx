296c7183c07907de2eb51fdfd12ac21be32951d3
pomahcku <rom2517pop@gmail.com>
2020-07-12 17:10:07 +0300

    add word-wrap and white space in posts representations.

--- src/components/posts/section/Sections.jsx
+++ src/components/posts/section/Sections.jsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import {Col, Row} from "reactstrap";
+import style from "../Posts.module.css";
 
 let Sections = (props) => {
     let sections = props.sections.map(section => {
             return (
                 <Row key={section.id}>
-                    <Col>
+                    <Col className={style.article}>
                         {section.article}
                     </Col>
                 </Row>
