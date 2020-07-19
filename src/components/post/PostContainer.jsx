8aa74b8981e62204332b2a929af1b39565cff71a
pomahcku <rom2517pop@gmail.com>
2020-07-19 13:22:39 +0300

    Rendered tags and genres in Post.jsx

--- src/components/post/PostContainer.jsx
+++ src/components/post/PostContainer.jsx
@@ -2,13 +2,16 @@ import * as React from "react";
 import {Col, Row} from "reactstrap";
 import Post from "./Post";
 import {connect} from "react-redux";
+import {getByGenre, getByTag} from "../../redux/PostReducer";
 
 const PostContainer = (props) => {
 
     return (
         <Row>
             <Col>
-                <Post post={props.post}/>
+                <Post post={props.post}
+                      getByGenre={props.getByGenre}
+                      getByTag={props.getByTag}/>
             </Col>
         </Row>
     )
@@ -18,6 +21,9 @@ const mapStateToProps = (state) => ({
     post: state.postsState.openedPost,
 })
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = {
+    getByGenre,
+    getByTag
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostContainer);
