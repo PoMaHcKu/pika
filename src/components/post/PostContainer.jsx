e8ed55ee8a8843b62cac19d172fede3906f7adc4
r.popov <rom2517pop@gmail.com>
2020-07-25 16:11:04 +0300

    show preloader if new post is creating.

--- src/components/post/PostContainer.jsx
+++ src/components/post/PostContainer.jsx
@@ -1,24 +1,27 @@
 import * as React from "react";
 import {Col, Row} from "reactstrap";
 import Post from "./Post";
-import {connect} from "react-redux";
 import {getByGenre, getByTag} from "../../redux/PostReducer";
+import Preloader from "../common/preloader/Preloader";
+import {connect} from "react-redux";
 
 const PostContainer = (props) => {
 
     return (
-        <Row>
-            <Col>
-                <Post post={props.post}
-                      getByGenre={props.getByGenre}
-                      getByTag={props.getByTag}/>
-            </Col>
-        </Row>
+        props.creatingNewPost ? <Preloader/> :
+            <Row>
+                <Col>
+                    <Post post={props.post}
+                          getByGenre={props.getByGenre}
+                          getByTag={props.getByTag}/>
+                </Col>
+            </Row>
     )
 }
 
 const mapStateToProps = (state) => ({
     post: state.postsState.openedPost,
+    creatingNewPost: state.newPostState.isLoading
 })
 
 const mapDispatchToProps = {
--- src/redux/NewPostReducer.js
+++ src/redux/NewPostReducer.js
@@ -1,53 +1,68 @@
-import {GenreDao} from "../dao/GenreDao";
-import {PostDao} from "../dao/PostDao";
-import {setOpenedPost} from "./PostReducer";
+import {GenreDao} from "../dao/GenreDao"
+import {PostDao} from "../dao/PostDao"
+import {setOpenedPost} from "./PostReducer"
 
-const SET_GENRES = "SET-GENRES";
+const SET_GENRES = "SET-GENRES"
+const CHANGE_LOAD_STATUS = "CHANGE-LOAD-STATUS"
 
 const defaultState = {
+    isLoading: false,
     genres: [],
 }
 
 const newPostReducer = (state = defaultState, action) => {
     switch (action.type) {
+        case CHANGE_LOAD_STATUS:
+            return {
+                ...state,
+                isLoading: action.status,
+            }
         case SET_GENRES:
             return {
                 ...state,
                 genres: [...action.genres],
             }
         default:
-            return state;
+            return state
     }
 }
+
+const changeLoadStatus = (status) => ({
+    type: CHANGE_LOAD_STATUS,
+    status
+})
 
 export const setGenres = (genres) => ({
     type: SET_GENRES,
     genres
 })
 
-const genreDao = new GenreDao();
-const postDao = new PostDao();
+const genreDao = new GenreDao()
+const postDao = new PostDao()
 
 export const getGenres = () => dispatch => {
     return genreDao.getGenres()
         .then(response => {
             if (response.data) {
-                dispatch(setGenres(response.data));
+                dispatch(setGenres(response.data))
             } else {
-                alert(response);
+                alert(response)
             }
-        });
+        })
 }
 
 export const createPost = (post) => dispatch => {
+    dispatch(changeLoadStatus(true))
     return postDao.createPost(post)
         .then(response => {
             if (response && response.data) {
-                dispatch(setOpenedPost(response.data));
+                debugger
+                dispatch(setOpenedPost(response.data))
+                dispatch(changeLoadStatus(false))
             } else {
-                alert(response);
+                alert(response)
             }
-        });
+        })
 }
 
-export default newPostReducer;
+export default newPostReducer
