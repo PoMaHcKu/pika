7f9cb3fde6f4c30f6dadd4a0263d1ce5752e02d0
pomahcku <rom2517pop@gmail.com>
2020-07-26 23:55:12 +0300

    changed redirect path in WithAuthRedirect.

--- src/hoc/WithRedirectToMain.js
+++ src/hoc/WithRedirectToMain.js
@@ -13,7 +13,7 @@ export const withAuthRedirect = (Component) => {
     class RedirectComponent extends React.Component {
         render() {
             if (this.props.isAuth) {
-                return <Redirect to={"/main"}/>
+                return <Redirect to={"/"}/>
             }
             return <Component {...this.props}/>
         }
